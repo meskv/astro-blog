00a5962e5e93933305324b9d1c6bd04dfee3883e
Suman <krsk1495@gmail.com>
2026-09-21 16:17:50 +0530

    Improve blog layout and add Google verification

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
